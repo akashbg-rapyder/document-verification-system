803b1edf03bddd8486ddba7d704f38906d1da0ae
pavankumarvs76 <pavankumar76@gmail.com>
2023-03-17 01:08:29 +0530

    Ui Development V0.1 reupload

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+import './signup.css'
+import { Link } from 'react-router-dom';
+import './Services'
+
+export default function SignUp() {
+  return <div className='sign-up'>
+    <form class="box" action="index.html" method="post">
+      <h1 className='boxH1'>VALIDATE</h1>
+      <input type="text" name="" placeholder="Username"/>
+      <input type="password" name="" placeholder="Password"/>
+      <Link to="/services" className='btnLogin'>Sign up</Link>
+    </form>
+  </div>;
+}
