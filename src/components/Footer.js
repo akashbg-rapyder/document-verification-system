8304332da806e531f93caec3c16c78ab62954f54
Akash B  G <akash.bg@corp.rapyder.com>
2023-03-17 02:08:17 +0530

    Ui Development V0.2- rebase

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
+          Join the Adventure to get validate and yourself!
         </p>
         <p className='footer-subscription-text'>
           You can unsubscribe at any time.
@@ -31,7 +31,6 @@ function Footer() {
             <h2>About Us</h2>
             <Link to='/sign-up'>How it works</Link>
             <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
             <Link to='/'>Investors</Link>
             <Link to='/'>Terms of Service</Link>
           </div>
@@ -45,13 +44,6 @@ function Footer() {
         </div>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
-          </div>
-          <div class='footer-link-items'>
             <h2>Social Media</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
@@ -64,11 +56,11 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
+              VERIDIFI
               <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
+          <small class='website-rights'>VERIDIFI © 2023</small>
           <div class='social-icons'>
             <Link
               class='social-icon-link facebook'
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import '../../App.css';
+import './services.css'
+import { Link } from 'react-router-dom';
+import './Services'
 
 export default function Services() {
-  return <h1 className='services'>SERVICES</h1>;
+  return <div className='sign-up'>
+  <form class="box" action="index.html" method="post">
+    <h1 className='boxH1'>VALIDATE</h1>
+    <input type="text" name="" placeholder="First Name"/>
+    <input type="text" name="" placeholder="Last Name"/>
+    <div class="custom-select">
+    <select>
+      <option value="0">Select ID:</option>
+      <option value="1">Aadhar Card</option>
+      <option value="2">PAN Card</option>
+      <option value="3">Driving Licence</option>
+    </select>
+  </div>
+    <input type="number" name="" placeholder="ID Namber"/>
+    <Link to="/services" className='btnLogin'>Sign up</Link>
+  </form>
+</div>;
 }
