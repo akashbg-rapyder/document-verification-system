d1a8906e4f406b37328bd381cb54745a5f697dbc
pavankumarvs76 <pavankumar76@gmail.com>
2023-03-17 13:46:37 +0530

    Ui Development V0.2 Update

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -6,20 +6,31 @@ import './Services'
 
 export default function Services() {
   return <div className='sign-up'>
-  <form class="box" action="index.html" method="post">
+  <form class="Servicebox" action="index.html" method="post">
     <h1 className='boxH1'>VALIDATE</h1>
+    <div className="name">
     <input type="text" name="" placeholder="First Name"/>
     <input type="text" name="" placeholder="Last Name"/>
+
+    </div>
     <div class="custom-select">
-    <select>
+    <select className='serviceSelect'>
       <option value="0">Select ID:</option>
       <option value="1">Aadhar Card</option>
       <option value="2">PAN Card</option>
       <option value="3">Driving Licence</option>
     </select>
+    <input className='serviceSelect' type="number" name="" placeholder="ID Namber"/>
+    <select className='serviceGender'>
+      <option value="0">Male</option>
+      <option value="1">Female</option>
+    </select>
   </div>
-    <input type="number" name="" placeholder="ID Namber"/>
+  <div className='FileSubmit'>
+  <input type="file" id="myFile" name="filename"/>
     <Link to="/services" className='btnLogin'>Sign up</Link>
+  </div>
+    
   </form>
 </div>;
 }
